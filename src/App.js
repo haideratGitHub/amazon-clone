2783766e1d9415745a31687220897ea0aa098e65
Haider <haiderabbas1982000@gmail.com>
2020-10-05 21:44:26 +0500

    implemeted and styled Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world, let's buid fucking amazon clone </h1>
+      {/* Header */}
+      <Header></Header>
+      {/* Home */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
+      ></img>
+      <div className="header__search">
+        <input className="header_searchInput" type="text"></input>
+        <SearchIcon className="header__searchIcon"></SearchIcon>
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionOne">Hello Guest</span>
+          <span className="header__optionTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionOne">Returns</span>
+          <span className="header__optionTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionOne">Your</span>
+          <span className="header__optionTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon></ShoppingBasketIcon>
+          <span className="header__optionTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Header;
