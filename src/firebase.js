2cd2ec0ea1ace1d2ad3f5c59ae9f907a908402ec
Haider <haiderabbas1982000@gmail.com>
2020-10-05 20:18:25 +0500

    initial project setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzp3L25es1pYnep3dOJIBsDpONP64F4K8",
+  authDomain: "clone-cceda.firebaseapp.com",
+  databaseURL: "https://clone-cceda.firebaseio.com",
+  projectId: "clone-cceda",
+  storageBucket: "clone-cceda.appspot.com",
+  messagingSenderId: "378062369511",
+  appId: "1:378062369511:web:9a0c71b7d0b7c0a27ca842",
+  measurementId: "G-2HF2FMMTZ1",
+};
