a869fe55cdcada06d401b4ea15623547ad1c1153
Haider <haiderabbas1982000@gmail.com>
2020-10-06 20:45:18 +0500

    Added mock products and styling

--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,23 @@
 import React from "react";
 import "./Product.css";
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
-          <p>🌟</p>
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-        alt=""
-      ></img>
+      <img src={image} alt=""></img>
       <button>Add to Basket</button>
     </div>
   );
