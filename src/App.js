bba3c8ef46c8db340e2a48bc9671c828d9ce3142
Haider <haiderabbas1982000@gmail.com>
2020-10-07 11:23:52 +0500

    Added routing to checkout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
+import Checkout from "./Checkout.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <Header></Header>
-      {/* Home */}
-      <Home></Home>
-    </div>
+    //BEM notation for naming convention
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Checkout></Checkout>
+          </Route>
+          <Route path="/">
+            <Header></Header>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Checkout.css";
+function checkout() {
+  return (
+    <div>
+      <h1>testing checkout routing</h1>
+    </div>
+  );
+}
+
+export default checkout;
